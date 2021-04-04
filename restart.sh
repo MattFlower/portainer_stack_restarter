e45cb5d13d8663fddeffe13f0c053112aac341e4
Matthew Flower <mflower@squarespace.com>
2021-04-04 10:12:47 -0400

    Fixed readme, make restart a little bit quieter and fix bugs

--- restart.sh
+++ restart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 if [[ -z "$PORTAINER_USERNAME" ]]; then
     echo "Please define an environment variable named PORTAINER_USERNAME"
     exit 1
@@ -21,13 +23,13 @@ if [[ -z "$SERVER_AND_PORT" ]]; then
 fi 
 
 echo "Logging into portainer at $SERVER_AND_PORT"
-JWT_TOKEN=$(http POST $SERVER_AND_PORT/api/auth Username="$PORTAINER_USERNAME" Password="$PORTAINER_PASSWORD" | jq .jwt | tr -d '"')
+JWT_TOKEN=$(http --ignore-stdin POST $SERVER_AND_PORT/api/auth Username="$PORTAINER_USERNAME" Password="$PORTAINER_PASSWORD" | jq .jwt | tr -d '"')
 
 echo "Getting list of all stacks and filtering to find $APP_TO_RESTART"
-ID=$(http GET $SERVER_AND_PORT/api/stacks "Authorization: Bearer $JWT_TOKEN" | jq ".[] | select(.Name == '$APP_TO_RESTART') | .Id")
+ID=$(http --ignore-stdin GET $SERVER_AND_PORT/api/stacks "Authorization: Bearer $JWT_TOKEN" | jq ".[] | select(.Name == \"$APP_TO_RESTART\") | .Id")
 
 echo "Stopping $APP_TO_RESTART"
-http POST $SERVER_AND_PORT/api/stacks/$ID/stop "Authorization: Bearer $JWT_TOKEN"
+http --quiet --ignore-stdin POST $SERVER_AND_PORT/api/stacks/$ID/stop "Authorization: Bearer $JWT_TOKEN"
 
 echo "Starting $APP_TO_RESTART"
-http POST $SERVER_AND_PORT/api/stacks/$ID/start "Authorization: Bearer $JWT_TOKEN"
+http --quiet --ignore-stdin POST $SERVER_AND_PORT/api/stacks/$ID/start "Authorization: Bearer $JWT_TOKEN"
